efec993b770af954218bcb5d3e765cdf144be66d
Lai Yan Kai <lai.yankai@gmail.com>
2025-12-11 14:54:22 +0800

    1st test: update train_yolo.py

--- train_yolo/train_yolo.py
+++ train_yolo/train_yolo.py
@@ -5,7 +5,7 @@ model = YOLO("yolo11n.pt")
 
 # Train the model
 train_results = model.train(
-    data="dataset.yaml",  # path to dataset YAML
+    data="datasets.yaml",  # path to dataset YAML
     epochs=100,  # number of training epochs
     imgsz=320,  # All images are automatically resized before training and predicting.
     device="cpu",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
@@ -14,5 +14,5 @@ train_results = model.train(
 # Evaluate model performance on the validation set
 metrics = model.val()
 
-# Export the model to ONNX format
-path = model.export(format="onnx")  # return path to exported model
+# Export the model
+path = model.export()  # return path to exported model
